41e61334ab50b1f1d2db6356c2707ba11e12c6f1
chao.diangen <chao.diangen@trs.com.cn>
2022-08-26 15:16:47 +0800

    项目报错

--- src/db/db.module.ts
+++ src/db/db.module.ts
@@ -3,16 +3,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '123456',
-      database: 'test',
-      entities: [],
-      synchronize: true,
-    }),
+    // TypeOrmModule.forRoot({
+    //   type: 'mysql',
+    //   host: 'localhost',
+    //   port: 3306,
+    //   username: 'root',
+    //   password: '123456',
+    //   database: 'export',
+    //   entities: [],
+    //   synchronize: true,
+    // }),
   ],
 })
 export class DbModule {}
